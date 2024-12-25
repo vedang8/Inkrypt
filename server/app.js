f11b163df7bbeb8725749a2b445b50d593a0f65f
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-12-25 08:29:44 +0530

    initialized the routes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Routes
+
 const PORT = 7000;
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
