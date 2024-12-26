c0690a618379b208c07fcba0bf9bda30b84d3185
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-12-26 19:10:36 +0530

    Logout router is created

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,5 +6,6 @@ const authenticate = require("../middleware/auth");
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/validuser', authenticate, userController.validUser);
+router.get('/logout', authenticate, userController.logOut);
 
 module.exports = router;
