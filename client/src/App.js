228aee89c9fa7df56bb67a6dd0bef222a3ea7a66
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-12-27 16:10:13 +0530

    added the routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Register from './components/Register/Register';
 import Dashboard  from './components/Dashboard/Dashboard';
 import {useSelector} from "react-redux";
 import Spinner from './utils/Spinner';
+import NoteDisplay from './components/NoteDisplay/NoteDisplay';
+import NoteEditor from './components/NoteEditor/NoteEditor';
+
 function App() {
   const loading = useSelector(state=>state.loader.value);
   return (
@@ -12,8 +15,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element = {<Login />} />
-        <Route path="/register" element = {<Register/>} />
-        <Route path="/home" element = {<Dashboard/>}  />
+        <Route path="/register" element = {<Register />} />
+        <Route path="/home" element = {<Dashboard />}  />
+        <Route path="/note-display/:noteId" element = {<NoteDisplay />} />
+        <Route path="/note/edit/:noteId" element = {<NoteEditor/>} />
       </Routes>
     </BrowserRouter>
     </>
